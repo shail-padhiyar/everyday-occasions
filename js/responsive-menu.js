356538b57a1e5c235399cc80f3d1f3f6407c6b6e
shail-padhiyar <65963916+shail-padhiyar@users.noreply.github.com>
2025-02-09 23:06:19 +0530

    Adding all files

---
+++ js/responsive-menu.js
@@ -0,0 +1,109 @@
+const menu = document.querySelector(".menu");
+const menuInner = menu.querySelector(".menu-inner");
+const menuArrow = menu.querySelector(".menu-arrow");
+const burger = document.querySelector(".burger");
+const overlay = document.querySelector(".overlay");
+
+// Navbar Menu Toggle Function
+function toggleMenu() {
+    menu.classList.toggle("is-active");
+    overlay.classList.toggle("is-active");
+}
+
+// Show Mobile Submenu Function
+function showSubMenu(children) {
+    subMenu = children.querySelector(".submenu");
+    subMenu.classList.add("is-active");
+    subMenu.style.animation = "slideLeft 0.35s ease forwards";
+    const menuTitle = children.querySelector("i").parentNode.childNodes[0]
+        .textContent;
+    menu.querySelector(".menu-title").textContent = menuTitle;
+    menu.querySelector(".menu-header").classList.add("is-active");
+}
+
+// Hide Mobile Submenu Function
+function hideSubMenu() {
+    subMenu.style.animation = "slideRight 0.35s ease forwards";
+    setTimeout(() => {
+        subMenu.classList.remove("is-active");
+    }, 300);
+
+    menu.querySelector(".menu-title").textContent = "";
+    menu.querySelector(".menu-header").classList.remove("is-active");
+}
+
+// Toggle Mobile Submenu Function
+function toggleSubMenu(e) {
+    if (!menu.classList.contains("is-active")) {
+        return;
+    }
+    if (e.target.closest(".menu-dropdown")) {
+        const children = e.target.closest(".menu-dropdown");
+        showSubMenu(children);
+    }
+}
+
+// Fixed Navbar Menu on Window Resize
+window.addEventListener("resize", () => {
+    if (window.innerWidth >= 992) {
+        if (menu.classList.contains("is-active")) {
+            toggleMenu();
+        }
+    }
+});
+
+// Initialize All Event Listeners
+burger.addEventListener("click", toggleMenu);
+overlay.addEventListener("click", toggleMenu);
+menuArrow.addEventListener("click", hideSubMenu);
+menuInner.addEventListener("click", toggleSubMenu);
+
+
+$('.burger, .overlay').on('click', function() {
+    // Check if #menu has the .is-active class
+    if ($('#menu').hasClass('is-active')) {
+        // If it has, remove the .is-active class and add .close class to .burger
+        $('.burger').addClass('close');
+        $('body').addClass('overflow-hidden');
+    } else {
+        // If it doesn't have, add the .is-active class and remove .close class from .burger
+        $('.burger').removeClass('close');
+        $('body').removeClass('overflow-hidden');
+    }
+});
+
+let mybutton = document.getElementById("burger");
+
+// When the user scrolls down 20px from the top of the document, show the button
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         mybutton.style.display = "block";
+//     } else {
+//         mybutton.style.display = "none";
+//     }
+// }
+
+// When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//     document.body.scrollTop = 0; // For Safari
+//     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
+
+$(document).ready(function() {
+    function adjustMainMargin() {
+        var headerHeight = $('.header-section').outerHeight();
+        $('main').css('margin-top', headerHeight + 'px');
+    }
+
+    // Adjust margin on load
+    adjustMainMargin();
+
+    // Adjust margin on window resize
+    $(window).resize(function() {
+        adjustMainMargin();
+    });
+});
+
